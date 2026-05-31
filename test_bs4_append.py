1241e59fa13569f929fbe4484ce9eb1630f52d38
ider <lu.hongfei@outlook.com>
2026-05-31 17:03:55 +0800

    chore: remove temporary debug scripts

--- test_bs4_append.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup("<table><tr></tr></table>", "html.parser")
-tr = soup.tr
-content = "<b>Bold</b> and <img src='test.png'>"
-new_cell = soup.new_tag("td")
-new_cell.append(BeautifulSoup(content, "html.parser"))
-tr.append(new_cell)
-print(str(tr))
